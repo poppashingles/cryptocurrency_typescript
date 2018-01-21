ea3a4b75f1bb9cc02145b3b67c7f674c4dee80e5
Rob <robdwilliams85@gmail.com>
2018-01-21 17:41:43 +0000

    Two new methods validate the structure and contents of any new block added to the chain

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -30,4 +30,27 @@ class Block {
     const newBlock: Block = new Block(nextIndex, nextHash, previousBlock.hash, nextTimeStamp, blockData);
     return newBlock;
   }
+
+  const isValidNewBlock = (newBlock: Block, previousBlock: Block) => {
+    if (previousBlock.index + 1 !== newBlock.index) {
+      console.log('Invalid index');
+      return false;
+    } else if (previousBlock.hash !== newBlock.previousHash) {
+      console.log('Invalid previous hash');
+      return false;
+    } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
+      console.log(typeof (newBlock.hash) + ' ' + typeof calculateHashForBlock(newBlock));
+      console.log('Invalid hash: ' + calculateHashForBlock(newBlock) + ' ' + newBlock.hash);
+      return false;
+    }
+    return true;
+  }
+
+  const isValidBlockStructure = (block:Block): boolean => {
+    return   typeof block.index === 'number'
+          && typeof block.hash === 'string'
+          && typeof block.previousHash === 'string'
+          && typeof block.timestamp === 'number'
+          && typeof block.data === 'string';
+  }
 }
